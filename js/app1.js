a48ce37000c74c52a62c2d941d68b1f82b400e6b
Brhymer <leiqien28@hotmail.com>
2020-04-06 14:35:32 -0700

    adds constants to js files

---
+++ js/app1.js
@@ -0,0 +1,63 @@
+// Opening page -- no enemies
+
+const linkStats = {
+    hp: 150,
+    weapon: "none",
+    damage: "20"
+}
+
+const linkPos = {
+    x: 6, y: 6
+};
+
+const entrance = {
+    x: 5, y: 1
+}
+
+const oldManPos =
+    {x: 4, y: 4};
+
+const treasures = [
+    {x: 2, y: 6},
+    {x: 3, y: 3}
+];
+
+const rocks =[
+    {x: 0, y: 0},
+    {x: 0, y: 1},
+    {x: 0, y: 2},
+    {x: 0, y: 3},
+    {x: 0, y: 4},
+    {x: 0, y: 5},
+    {x: 0, y: 6},
+    {x: 1, y: 0},
+    {x: 1, y: 1},
+    {x: 1, y: 2},
+    {x: 1, y: 3},
+    {x: 1, y: 4},
+    {x: 1, y: 5},
+    {x: 1, y: 6},
+    {x: 2, y: 0},
+    {x: 2, y: 1},
+    {x: 3, y: 0},
+    {x: 3, y: 1},
+    {x: 4, y: 0},
+    {x: 4, y: 1},
+    {x: 5, y: 0},
+    {x: 6, y: 0},
+    {x: 6, y: 1},
+    {x: 7, y: 0},
+    {x: 7, y: 1},
+    {x: 7, y: 2},
+    {x: 8, y: 0},
+    {x: 8, y: 1},
+    {x: 8, y: 2},
+    {x: 8, y: 3},
+    {x: 9, y: 0},
+    {x: 9, y: 1},
+    {x: 9, y: 2},
+    {x: 9, y: 3},
+    {x: 9, y: 4},
+    {x: 9, y: 5},
+    {x: 9, y: 6},
+];
